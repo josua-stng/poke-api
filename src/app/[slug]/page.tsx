ae23614177ac61a1916589cc13eb0dee190c3df0
josua-stng <josuasitanggang51@gmail.com>
2024-07-02 12:56:27 +0700

    feat: add compare pokemon in pokemon detail page

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,12 +1,45 @@
+'use client';
+import Link from 'next/link';
+import usePokemonDetail from '../query/pokemon/use-fetch-pokemon-detail';
+import KeyboardBackspaceIcon from '@mui/icons-material/KeyboardBackspace';
+import PokemonDetail from './pokemon_detail';
+import { useSearchParams } from 'next/navigation';
+import ComparePokemon from '../compare-pokemon/compare-pokemon';
+import LoadingSkeletonPokemonDetail from '../loading-skeleton/loading-skeleton-pokemon-detail';
+
 type slug = {
   params: {
-    slug: number;
+    slug: string;
   };
 };
+
 export default function PokemonDetailPage({ params: { slug } }: slug) {
+  const searchParams = useSearchParams();
+  const paramsCompare = searchParams.get('compare');
+
+  const { data: pokemon_detail, isPending } = usePokemonDetail(slug);
+  if (isPending) {
+    return <LoadingSkeletonPokemonDetail />;
+  }
+
   return (
-    <div>
-      <p>pokemon detail {slug}</p>
-    </div>
+    <main>
+      <Link
+        href={'/'}
+        className="flex items-center m-3 gap-2 bg-gray-300 w-max px-3 py-1.5 rounded-md border border-gray-400 hover:bg-gray-400 hover:text-white cursor-pointer "
+      >
+        <KeyboardBackspaceIcon />
+        <p>Back</p>
+      </Link>
+      <div className="md:flex justify-center   max-w-5xl space-y-5 md:space-y-0  mx-auto">
+        <PokemonDetail
+          pokemon_name={slug}
+          pokemon_image={pokemon_detail.sprites.front_default}
+          pokemon_type={pokemon_detail.types[0].type.name}
+          pokemon_stats={pokemon_detail.stats}
+        />
+        {paramsCompare ? <ComparePokemon pokemon_name={paramsCompare} /> : null}
+      </div>
+    </main>
   );
 }
