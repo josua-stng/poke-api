ffdd5fe0f80c355c69a58662e24eb74386392b45
josua-stng <josuasitanggang51@gmail.com>
2024-07-03 09:05:39 +0700

    feat: add not available pokemon when save pokemon empty

---
+++ src/app/my-pokemon/not-available-pokemon.tsx
@@ -0,0 +1,9 @@
+export default function NotAvailablePokemon() {
+  return (
+    <div className="flex items-center justify-center min-h-[80vh]">
+      <h1 className="text-center font-bold text-2xl text-red-500 italic font-sans">
+        Saved Pokemon is not available
+      </h1>
+    </div>
+  );
+}
